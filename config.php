0cecdfae572dfa2e3f748dbe6a661c89f21e0f8e
Stefk <58315744+stefk97@users.noreply.github.com>
2022-04-29 22:29:35 -0700

    Update config.php

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'demo');
+define('DB_USERNAME', 'beforeresturant_user');
+define('DB_PASSWORD', 'BeforeResturantLogin1234');
+define('DB_NAME', 'beforeresturant');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
